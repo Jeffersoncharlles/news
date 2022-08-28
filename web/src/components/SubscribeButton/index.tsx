15b656fe38ce2b8f6a96342b7c857f2222960aa2
Jefferson Charlles <user@Jefferson>
2022-08-27 23:12:56 -0300

    chore:create layout home

---
+++ web/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss'
+
+export const SubscribeButton = () => {
+
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+}
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import styles from './index.module.scss'
 
 
 const Home: NextPage = () => {
@@ -8,9 +9,17 @@ const Home: NextPage = () => {
             <Head>
                 <title>Inicio | News</title>
             </Head>
-            <div>
-                <h1>Hello World</h1>
-            </div>
+            <main className={styles.container}>
+                <section className={styles.container_content}>
+                    <span>👏 Hey, welcome</span>
+                    <h1>News about <br />the <span>React</span> world</h1>
+                    <p>Get access to all the publications <br />
+                        <span>for $9.90 month</span>
+                    </p>
+                </section>
+
+                <img src="/images/avatar.svg" alt="Girl coding" />
+            </main>
         </>
     )
 }
