24a66867543a4c2cdbfcbd5b6b61a69fbfb2d5f7
Jefferson Charlles <user@Jefferson>
2022-08-27 22:57:25 -0300

    chore:create layout button signin

--- web/src/components/SignInButton/index.tsx
+++ web/src/components/SignInButton/index.tsx
@@ -1,10 +1,20 @@
+import { GoogleLogo, X } from 'phosphor-react';
 import styles from './styles.module.scss'
 
 export const SignInButton = () => {
 
-    return (
-        <div className={styles.container}>
+    const isUserLoggedIn = true;
 
-        </div>
+    return isUserLoggedIn ? (
+        <button type='button' className={styles.container}>
+            <GoogleLogo size={20} weight="bold" color='#04D361' />
+            Jefferson Charlles
+            <X size={20} weight="bold" color='#737380' />
+        </button>
+    ) : (
+        <button type='button' className={styles.container}>
+            <GoogleLogo size={20} weight="bold" color='#eba417' />
+            Sign in with Google
+        </button>
     );
 }
